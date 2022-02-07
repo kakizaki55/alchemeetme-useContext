65de06c0c4e4cb451648cb5aae5b64da7f04a309
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-07 12:45:43 -0800

    just cleaning up some console.log

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,8 +2,9 @@ import { useUser } from '../../context/UserContext'
 import LikeList from '../LikeList/LikeList'
 
 export default function Profile() {
-  const { user } = useUser()
-  const { name, avatar, likes, motto, color, header } = user
+  const {
+    user: { name, avatar, likes, motto, color, header },
+  } = useUser()
   return (
     <div className="bg-gray-100 bg-opacity-95 my-12 pb-6 w-full justify-center items-center overflow-hidden md:max-w-sm rounded-lg shadow-sm mx-auto">
       <div className="relative h-40">
